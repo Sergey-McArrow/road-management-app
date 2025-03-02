5749b39716f6d6c53ea35ca5366a50adc3e947c3
Sergey-McArrow <s.makarov.de@gmail.com>
2025-03-02 17:07:30 +0100

    fix: use built-in CORS in json-server

--- db-road-management-app/cors.js
+++ db-road-management-app/cors.js
@@ -1,6 +1,6 @@
-export default (req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Allow-Methods', '*');
-  next();
+module.exports = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', '*')
+  res.header('Access-Control-Allow-Methods', '*')
+  next()
 }

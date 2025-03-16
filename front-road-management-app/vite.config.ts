c1fd0fabbdaf4c86bf155471ef1350666650dd38
Sergey-McArrow <s.makarov.de@gmail.com>
2025-03-16 16:26:57 +0100

    add proxy for backend

--- front-road-management-app/vite.config.ts
+++ front-road-management-app/vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from "path"
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://18.192.25.99:3001',
+        changeOrigin: true,
+        secure: false,
+      },
     },
   },
 })
